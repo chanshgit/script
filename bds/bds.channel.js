702816f61c3dafcc203c22c64f9fe8b94cc1b617
chanshgit <65504961+chanshgit@users.noreply.github.com>
2022-05-07 16:48:27 +0800

    Create bds.channel.js

---
+++ bds/bds.channel.js
@@ -0,0 +1,213 @@
+var body = {
+  "message" : "success",
+  "data" : {
+    "channel_model" : [
+      {
+        "parent_channel" : 9999,
+        "extra" : null,
+        "webview" : null,
+        "event_name" : "follow_feed",
+        "url" : "",
+        "list_name" : "关注",
+        "list_view_type" : 0,
+        "pangle_rit" : null,
+        "color_conf" : null,
+        "is_immersive" : false,
+        "list_type" : 2,
+        "sub_channel_list" : null,
+        "title_style_conf" : null
+      },
+      {
+        "parent_channel" : 9999,
+        "extra" : null,
+        "webview" : null,
+        "event_name" : "feed",
+        "url" : "",
+        "list_name" : "推荐",
+        "list_view_type" : 0,
+        "pangle_rit" : null,
+        "color_conf" : null,
+        "is_immersive" : false,
+        "list_type" : 1,
+        "sub_channel_list" : null,
+        "title_style_conf" : null
+      },
+      {
+        "parent_channel" : 9999,
+        "extra" : null,
+        "webview" : null,
+        "event_name" : "video",
+        "url" : "",
+        "list_name" : "视频",
+        "list_view_type" : 0,
+        "pangle_rit" : null,
+        "color_conf" : null,
+        "is_immersive" : false,
+        "list_type" : 4,
+        "sub_channel_list" : null,
+        "title_style_conf" : null
+      },
+      {
+        "parent_channel" : 9999,
+        "extra" : {
+          "city_id" : "510100",
+          "city_name" : "成都"
+        },
+        "webview" : null,
+        "event_name" : "city_chengdu",
+        "url" : "",
+        "list_name" : "成都",
+        "list_view_type" : 0,
+        "pangle_rit" : null,
+        "color_conf" : null,
+        "is_immersive" : false,
+        "list_type" : 53,
+        "sub_channel_list" : null,
+        "title_style_conf" : null
+      },/**
+      {
+        "parent_channel" : 9999,
+        "extra" : null,
+        "webview" : null,
+        "event_name" : "interaction",
+        "url" : "",
+        "list_name" : "虾聊",
+        "list_view_type" : 2,
+        "pangle_rit" : null,
+        "color_conf" : null,
+        "is_immersive" : false,
+        "list_type" : 52,
+        "sub_channel_list" : [
+
+        ],
+        "title_style_conf" : null
+      },
+      {
+        "parent_channel" : 52,
+        "extra" : null,
+        "webview" : null,
+        "event_name" : "interaction_hottest",
+        "url" : "",
+        "list_name" : "最热",
+        "list_view_type" : 0,
+        "pangle_rit" : null,
+        "color_conf" : null,
+        "is_immersive" : false,
+        "list_type" : 401,
+        "sub_channel_list" : null,
+        "title_style_conf" : null
+      },
+      {
+        "parent_channel" : 52,
+        "extra" : null,
+        "webview" : null,
+        "event_name" : "interaction_newest",
+        "url" : "",
+        "list_name" : "最新",
+        "list_view_type" : 0,
+        "pangle_rit" : null,
+        "color_conf" : null,
+        "is_immersive" : false,
+        "list_type" : 402,
+        "sub_channel_list" : null,
+        "title_style_conf" : null
+      },**/
+      {
+        "parent_channel" : 9999,
+        "extra" : null,
+        "webview" : null,
+        "event_name" : "girl",
+        "url" : "",
+        "list_name" : "颜值",
+        "list_view_type" : 0,
+        "pangle_rit" : null,
+        "color_conf" : null,
+        "is_immersive" : false,
+        "list_type" : 48,
+        "sub_channel_list" : null,
+        "title_style_conf" : null
+      },/**
+      {
+        "parent_channel" : 9999,
+        "extra" : null,
+        "webview" : null,
+        "event_name" : "life",
+        "url" : "",
+        "list_name" : "真香",
+        "list_view_type" : 0,
+        "pangle_rit" : null,
+        "color_conf" : null,
+        "is_immersive" : false,
+        "list_type" : 27,
+        "sub_channel_list" : null,
+        "title_style_conf" : null
+      },
+      {
+        "parent_channel" : 9999,
+        "extra" : null,
+        "webview" : null,
+        "event_name" : "car",
+        "url" : "",
+        "list_name" : "汽车",
+        "list_view_type" : 0,
+        "pangle_rit" : null,
+        "color_conf" : null,
+        "is_immersive" : false,
+        "list_type" : 28,
+        "sub_channel_list" : null,
+        "title_style_conf" : null
+      },**/
+      {
+        "parent_channel" : 9999,
+        "extra" : null,
+        "webview" : null,
+        "event_name" : "image",
+        "url" : "",
+        "list_name" : "图片",
+        "list_view_type" : 0,
+        "pangle_rit" : null,
+        "color_conf" : null,
+        "is_immersive" : false,
+        "list_type" : 10,
+        "sub_channel_list" : null,
+        "title_style_conf" : null
+      },/**
+      {
+        "parent_channel" : 9999,
+        "extra" : null,
+        "webview" : null,
+        "event_name" : "text",
+        "url" : "",
+        "list_name" : "文字",
+        "list_view_type" : 0,
+        "pangle_rit" : null,
+        "color_conf" : null,
+        "is_immersive" : false,
+        "list_type" : 11,
+        "sub_channel_list" : null,
+        "title_style_conf" : null
+      },**/
+      {
+        "parent_channel" : 9999,
+        "extra" : null,
+        "webview" : null,
+        "event_name" : "game_hub",
+        "url" : "",
+        "list_name" : "游戏",
+        "list_view_type" : 0,
+        "pangle_rit" : null,
+        "color_conf" : null,
+        "is_immersive" : false,
+        "list_type" : 16,
+        "sub_channel_list" : null,
+        "title_style_conf" : null
+      }
+    ],
+    "default_channel" : 1
+  },
+  "status_code" : 0,
+  "prompt" : "",
+  "time" : 1651912793220
+}
+var body = JSON.stringify(body);
+$done({body});
