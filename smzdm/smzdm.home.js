a50be947c11a824ee8e3de761258810d92c9c383
chanshgit <65504961+chanshgit@users.noreply.github.com>
2022-06-05 15:49:20 +0800

    Create smzdm.home.js

---
+++ smzdm/smzdm.home.js
@@ -0,0 +1,20 @@
+var body = JSON.parse($response.body);  //ToDo....
+var rows = body.data.rows;
+var newrows= [];
+for(var row of rows){
+  var val = 0;
+  if(row.article_interest){
+    var itst = article_interest;
+    val = parseInt(itst.article_worthy)/(parseInt(itst.article_worthy)+parseInt(itst.article_unworthy))
+  }else{
+    //9.3.3版本等旧版
+    val = parseInt(row.article_worthy)/(parseInt(row.article_worthy)+parseInt(row.article_unworthy));
+  }
+  //console.log(val); 
+  if(val*100 > 30){
+    newrows.push(row);
+  }
+}
+body.data.rows= newrows;
+ body = JSON.stringify(body);
+ $done({body}); 
