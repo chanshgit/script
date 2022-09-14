51f0a7ab938472154d12c31d86be2ed2953117ea
chanshgit <clinnestar@gmail.com>
2022-09-14 17:32:04 +0800

    修复图片无法下载和显示不全的问题

--- bds/bds.super.js
+++ bds/bds.super.js
@@ -24,64 +24,68 @@ Detail: /cell/detail
 var body = $response.body.replace(/id\":([0-9]{15,})/g, 'id":"$1str"');
 body = JSON.parse(body);
 
-if(body.data.data && body.data.data[0] && body.data.data[0].block_info){
-body.data.data = body.data.data[0].block_info.cell_list;
+if (body.data.data && body.data.data[0] && body.data.data[0].block_info) {
+  body.data.data = body.data.data[0].block_info.cell_list;
 }
 if (body.data.data) {
-    obj = body.data.data;
+  obj = body.data.data;
 } else if (body.data.replies) {
-    obj = body.data.replies;
+  obj = body.data.replies;
 } else if (body.data.cell_comments) {
-    obj = body.data.cell_comments;
+  obj = body.data.cell_comments;
 } else {
-    obj = null;
+  obj = null;
 }
 
 if (obj instanceof Array) {
-    if (obj != null) {
-        for (var i in obj) {
-            if (obj[i].ad_info != null) {
-                obj.splice(i, 1);
-            }
-            if (obj[i] && obj[i].item != null) {
-                if (obj[i].item.video != null) {
+  if (obj != null) {
+    for (var i in obj) {
+      if (obj[i].ad_info != null) {
+        obj.splice(i, 1);
+      }
+      if (obj[i] && obj[i].item != null) {
+        if (obj[i].item.video != null) {
                     obj[i].item.video.video_download.url_list = obj[i].item.origin_video_download.url_list;
-                }
-                for (var j in obj[i].item.comments) {
-                    if (obj[i].item.comments[j].video != null) {
+        }
+        for (var j in obj[i].item.comments) {
+          if (obj[i].item.comments[j].video != null) {
                         obj[i].item.comments[j].video_download.url_list = obj[i].item.comments[j].video.url_list;
-                    }
-                }
-            }
-            if (obj[i] && obj[i].comment_info != null) {
-                if (obj[i].comment_info.video != null) {
+          }
+        }
+      }
+      if (obj[i] && obj[i].comment_info != null) {
+        if (obj[i].comment_info.video != null) {
                     obj[i].comment_info.video_download.url_list = obj[i].comment_info.video.url_list;
-                }
-            }
         }
+      }
     }
+  }
 } else {
-    if (obj.item != null) {
-        if (obj.item.video != null) {
+  if (obj.item != null) {
+    if (obj.item.video != null) {
             obj.item.video.video_download.url_list = obj.item.origin_video_download.url_list;
-        }
-        for (var j in obj.item.comments) {
-            if (obj.item.comments[j].video != null) {
+    }
+    for (var j in obj.item.comments) {
+      if (obj.item.comments[j].video != null) {
                 obj.item.comments[j].video_download.url_list = obj.item.comments[j].video.url_list;
-            }
-        }
+      }
     }
-    if (obj.comment_info != null) {
-        if (obj.comment_info.video != null) {
+  }
+  if (obj.comment_info != null) {
+    if (obj.comment_info.video != null) {
             obj.comment_info.video_download.url_list = obj.comment_info.video.url_list;
-        }
     }
+  }
 }
 body = JSON.stringify(body);
 body = body.replace(/id\":\"([0-9]{15,})str\"/g, 'id":$1');
 body = body.replace(/\"can_download\":false/g, '"can_download":true');
-body = body.replace(/tplv-ppx-logo.image/g, '0x0.gif');
-body = body.replace(/tplv-ppx-logo/g, '0x0');
+//处理图片新域名会验证sign的问题
+//https://p6-ppx-sign.byteimg.com/tos-cn-i-0000/3e41f19d5c454d4094a8b1c008204f0c~tplv-f3gpralwbh-c5-v1:200:200:q80.jpeg?x-expires=1694308910&x-signature=MedZ0aEA%2FXgxRWVx0FpGshVm5ag%3D 
+//使用以前的请求方式
+//https://p6-ppx.byteimg.com/tos-cn-i-0000/3e41f19d5c454d4094a8b1c008204f0c~0x0.jpeg
+//0x0.jpeg,表示原图(无水印)
+body = body.replace(/-sign(\.byte.+?)tplv.+?\.(jpeg|webp|image|png).+?\"/g, '$10x0.$2"');
 $done({
-    body
+  body,
 });
