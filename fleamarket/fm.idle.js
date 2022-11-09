7fcb9bad7e6bf613678cdf5b9acefe2cfa8281d9
chanshgit <65504961+chanshgit@users.noreply.github.com>
2022-11-09 12:12:36 +0800

    Update fm.idle.js

--- fleamarket/fm.idle.js
+++ fleamarket/fm.idle.js
@@ -10,7 +10,7 @@ if(Object.keys(sData).length > 13){
     //console.log(uSketch);
     //满足条件：芝麻信用极好或无芝麻信用的
     var reg = new RegExp(/(neh|5KP)/);
-    if(!uSketch && !uSketch.topTags[0].tagUrl && reg.exec(uSketch.topTags[0].tagUrl))
+    if(!uSketch && !uSketch.topTags && !uSketch.topTags[0].tagUrl && reg.exec(uSketch.topTags[0].tagUrl))
 { 
         sections.push(card);
 }
