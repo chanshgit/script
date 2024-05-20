edba44ca22ea66837e313cfab175c582b9272aa3
chanshgit <clinnestar@gmail.com>
2024-05-20 22:33:06 +0800

    Add new feature:blacklist extends

--- fleamarket/v7.3/fm.other.js
+++ fleamarket/v7.3/fm.other.js
@@ -7,5 +7,10 @@ if(api=="mtop.taobao.idle.local.flow.plat.section"){
 else if(api=="mtop.taobao.wireless.home.xianyu.awesome.get"){
     obj.data.container.sections=obj.data.container.sections.splice(3,1);
 }
+else if(api=="mtop.taobao.idle.awesome.detail.unit"){
+    const sellerId = encodeURIComponent(obj.data.sellerDO.sellerId);
+    body = body.replace(/hcAppId/g, `sellerId=${sellerId}&hcAppId`);
+    obj = JSON.parse(body);
+}
 body = JSON.stringify(obj);
 $done({body});
--- fleamarket/v7.3/fm.search.js
+++ fleamarket/v7.3/fm.search.js
@@ -1,10 +1,13 @@
 var obj = JSON.parse($response.body);
+const ban_list = $prefs.valueForKey('ban_list');
 obj.data.resultList = obj.data.resultList.filter((result) => {
   var exContent = result.data.item.main.exContent;
   //屏蔽大家搜
   if(Object.keys(exContent).length < 20) return false;
   //屏蔽淘宝广告
-  if(exContent.isAliMaMaAD == "true") return false;
+  if(exContent.isAliMaMaAD === "true") return false;
+  //屏蔽黑名单
+  if(ban_list && ban_list.indexOf(result.data.item.main.clickParam.args.seller_id) != -1) return false;
   //信用极好标识
   return exContent.fishTags.r4 && /(0BT|5KP)/.test(exContent.fishTags.r4.tagList[0].data.url);
 });
