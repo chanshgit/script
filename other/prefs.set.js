15ba1f162eebe41442e4f660a4298f5de9cf0ff7
chanshgit <clinnestar@gmail.com>
2024-12-12 22:33:39 +0800

    修改上个版本的bug

--- other/prefs.set.js
+++ other/prefs.set.js
@@ -4,7 +4,12 @@ body = decodeURIComponent(body);
 const pathname = new URL(url).pathname.replace(/\//g,"_");
 //body = JSON.parse(body)
 //console.log(body.rules)
-console.log("配置："+ body);
+console.log(`配置(${pathname})：\n${body}`);
 
 $prefs.setValueForKey(body,pathname)
-$done({});
+const myHeaders = {
+    'Content-Type': 'application/json; charset=utf-8',
+    'Date': new Date().toUTCString(),
+};
+const myBody = JSON.stringify({msg: "success"});
+$done({ status: 'HTTP/1.1 200', headers:myHeaders, body: myBody });
